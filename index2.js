285a862e4b041ac0266abc979db4fe989a300216
Lovedeep Singh <748774@student.tor.loc>
2019-08-07 16:13:23 -0400

    third commit

--- index2.js
+++ index2.js
@@ -1,6 +1,9 @@
 const http=require('http');
 const site =http.createServer(function(req,res){
     console.log('Hello World');
-
+    //res.write("yeah Go HTTP!!");
+    res.setHeader('Content-Type','text/html');
+    console.log(req.rawHeaders);
+    res.end('<h1>Hello Class!</h1>');
 });
 site.listen(3000);
